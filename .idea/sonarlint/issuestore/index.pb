e866a23ecb3229a914b55cc1829dc69264067283
elossio <evlossio@gmail.com>
2023-07-24 15:53:12 -0300

    Ler contatos e escrever os e-mails

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 G
 Aula - RPA Python.ipynb,6\b\6bf55f62b2c55d817de544ca96ddae2bb6bbc6a2
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pyautogui
 import platform
 import time
+import pandas as pd
+import pyperclip
 
 
-class MeuRPA:
+class Imagem:
+    def __init__(self):
+        self.imagem = ''
+
+    def procurar_imagem(self, imagem: str):
+        self.imagem = imagem
+        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen(self.imagem)))
+
+
+class Contatos:
     def __init__(self):
         self.pausa = 1
         self.navegador = 'chrome'
@@ -23,16 +34,12 @@ class MeuRPA:
         pyautogui.write(self.navegador)
         pyautogui.press('enter')
 
-    def _procurar_imagem(self, imagem: str):
-        self.imagem = imagem
-        pyautogui.click(pyautogui.center(pyautogui.locateOnScreen(self.imagem)))
-
     def abrir_aplicacao(self, aplicacao):
         pyautogui.write(aplicacao)
         pyautogui.press('enter')
         while not pyautogui.locateOnScreen('busca_google.png'):
             time.sleep(1)
-        self._procurar_imagem(imagem='busca_google.png')
+        Imagem.procurar_imagem(self, imagem='busca_google.png')
         time.sleep(2)
         pyautogui.press('tab', presses=2)
         pyautogui.press('enter')
@@ -40,19 +47,55 @@ class MeuRPA:
             time.sleep(1)
 
     def _abrir_contatos(self):
-        self._procurar_imagem(imagem='pontinhos_menu.png')
-        self._procurar_imagem(imagem='contatos.png')
+        Imagem.procurar_imagem(self, imagem='pontinhos_menu.png')
+        Imagem.procurar_imagem(self, imagem='contatos.png')
         while not pyautogui.locateOnScreen('tela_contatos.png'):
             time.sleep(1)
 
     def exportar_contatos(self):
         self._abrir_contatos()
-        self._procurar_imagem(imagem='exportar.png')
-        self._procurar_imagem(imagem='confirmar_exportar.png')
+        Imagem.procurar_imagem(self, imagem='exportar.png')
+        Imagem.procurar_imagem(self, imagem='confirmar_exportar.png')
+
+
+class Correio:
+    def __init__(self):
+        self.aplicacao = 'gmail'
+
+    def _ler_contatos(self):
+        pyautogui.hotkey('ctrl', 'pgup')
+        return pd.read_csv(r'C://Users/evlos/Downloads/contacts.csv')
+
+    def escrever_email(self):
+        df = self._ler_contatos()
+        while not pyautogui.locateOnScreen('escrever.png'):
+            time.sleep(1)
+        for email in df['E-mail 1 - Value']:
+            Imagem.procurar_imagem(self, imagem='escrever.png')
+            pyautogui.write(email)
+            pyautogui.press('enter')
+            pyautogui.press('tab')
+            pyautogui.write('Teste de Envio de E-mail - DESCONSIDERAR O CONTEUDO')
+            pyautogui.press('tab')
+            texto = """
+            Coe Irmão,
+            
+            Beleza?
+            
+            Abraços"""
+            pyperclip.copy(texto)
+            pyautogui.hotkey('ctrl', 'v')
+            # descomentar para enviar os e-mails
+            # pyautogui.hotkey('ctrl', 'enter')
+
 
 if __name__ == '__main__':
-    controlador = MeuRPA()
-    controlador.abrir_navegador()
-    controlador.abrir_aplicacao('gmail')
-    controlador.exportar_contatos()
+    pyautogui.alert("O código vai começar. POr favor, não mexa em NADA enquanto o código tiver rodando! Eu aviso assim que terminar.")
+    contato = Contatos()
+    contato.abrir_navegador()
+    contato.abrir_aplicacao('gmail')
+    contato.exportar_contatos()
+    correio = Correio()
+    correio.escrever_email()
+    pyautogui.alert("O código terminou. Assuma o computador novamente!")
 
